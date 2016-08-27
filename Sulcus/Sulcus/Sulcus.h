c5bcd88b48cbc136703e98d23e24a2dad0b4a9c5
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-08-27 09:42:15 -0700

    Initial Commit

---
+++ Sulcus/Sulcus/Sulcus.h
@@ -0,0 +1,19 @@
+//
+//  Sulcus.h
+//  Sulcus
+//
+//  Created by Wellington Moreno on 8/27/16.
+//  Copyright © 2016 RedRoma, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sulcus.
+FOUNDATION_EXPORT double SulcusVersionNumber;
+
+//! Project version string for Sulcus.
+FOUNDATION_EXPORT const unsigned char SulcusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sulcus/PublicHeader.h>
+
+
